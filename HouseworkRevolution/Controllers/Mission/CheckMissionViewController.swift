12b57d718370e632952ba276da3a4a6648732289
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-08-29 16:40:36 +0800

    mission view UI

---
+++ HouseworkRevolution/Controllers/Mission/CheckMissionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CheckMissionViewController.swift
+//  HouseworkRevolution
+//
+//  Created by Sylvia Jia Fen  on 2019/8/29.
+//  Copyright © 2019 Sylvia Jia Fen . All rights reserved.
+//
+
+import UIKit
+
+class CheckMissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HouseworkRevolution/Controllers/Mission/MissionViewController.swift
@@ -0,0 +1,20 @@
+
+//
+//  MissionViewController.swift
+//  HouseworkRevolution
+//
+//  Created by Sylvia Jia Fen  on 2019/8/29.
+//  Copyright © 2019 Sylvia Jia Fen . All rights reserved.
+//
+
+import UIKit
+
+class MissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
