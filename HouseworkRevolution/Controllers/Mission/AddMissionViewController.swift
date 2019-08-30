b5c75b534afbed174e6c153fee43965a53e973bb
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-08-30 09:58:11 +0800

    set slideBar

--- HouseworkRevolution/Controllers/Mission/AddMissionViewController.swift
+++ HouseworkRevolution/Controllers/Mission/AddMissionViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 
 class AddMissionViewController: UIViewController {
 
+    @IBOutlet weak var addHouseworkBtn: UIButton!
+    
+    @IBOutlet weak var customHousework: UITextField!
+    
+    @IBOutlet weak var tiredSlider: UISlider!
+    
+    @IBOutlet weak var tiredValue: UILabel!
+    
+    @IBAction func userChangeTiredValue(_ sender: UISlider) {
+        
+        tiredValue.text = "\(Int(sender.value))"
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tiredSlider.setThumbImage(UIImage(named: "icons8-heart"), for: .normal)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    var houseworks: [String] = [] {
+        
+        didSet {
+            
+            setUpHouseworkBtn()
+            
+        }
     }
-    */
-
+    
+    func setUpHouseworkBtn() {
+        
+        if houseworks.count >= 12 {
+            
+            addHouseworkBtn.setTitle("移除", for: .normal)
+            
+            customHousework.isHidden = true
+            
+        } else {
+            
+            addHouseworkBtn.setTitle("新增家事標籤", for: .normal)
+            
+            customHousework.isHidden = false
+            
+        }
+    }
+    
 }
