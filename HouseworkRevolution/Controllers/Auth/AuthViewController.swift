f203669959f651ef9104d4287036709d4c9a0a78
Sylvia Jia Fen <kokoro.sharing@gmail.com>
2019-09-25 11:53:25 +0800

    change position of code in shouldChange... in Auth view

--- HouseworkRevolution/Controllers/Auth/AuthViewController.swift
+++ HouseworkRevolution/Controllers/Auth/AuthViewController.swift
@@ -335,13 +335,13 @@ extension AuthViewController: UITextFieldDelegate {
                    shouldChangeCharactersIn range: NSRange,
                    replacementString string: String) -> Bool {
         
-        let currentText = textField.text ?? ""
-        
+        if textField == customUserName || textField == createHomeName {
+            
+            let currentText = textField.text ?? ""
+            
             guard let stringRange = Range(range, in: currentText) else { return false }
 
             let updatedText = currentText.replacingCharacters(in: stringRange, with: string)
-        
-        if textField == customUserName || textField == createHomeName {
             
             return updatedText.count <= 6
         
